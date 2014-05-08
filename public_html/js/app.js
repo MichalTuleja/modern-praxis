afad81dafae3e8f50d88d69cb4da7137d04e6516
Michal Tuleja <michal.tuleja@outlook.com>
2014-05-08 08:08:55 +0200

    Demo version

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -5,17 +5,94 @@
  */
 
 var templates = [['dashboard.html', 'dashboard'],
-                 ['navigation_bar.html', 'navigation_bar'],
-                 ['patient_data.html', 'patient_data'],
-                 ['search.html', 'search'],
-                 ['table_list.html', 'table_list']];
+    ['navigation_bar.html', 'navigation_bar'],
+    ['patient_data.html', 'patient_data'],
+    ['search.html', 'search'],
+    ['table_list.html', 'table_list'],
+    ['calendar.html', 'calendar']];
 
 var promises = [];
 
-for(var i=0; i<templates.length; i++) {
+for (var i = 0; i < templates.length; i++) {
     promises.push(loadTemplate(templates[i][0], templates[i][1]));
 }
 
+var app = {};
+
+
+
+
+
+
 $.when.apply($, promises).then(function() {
-    $('#bodyContent').text('xD');
+   
+    app.Router = new Router();
+    Backbone.history.start();
+    
+    //switchTo('dashboard');
 });
+
+
+var switchTo = function(container) {
+    var containers = ['dashboard', 'table_list', 
+        'patient_data',
+    'calendar', 'report'];
+
+    for(var i=0; i<containers.length; i++) {
+        $('#' + containers[i]).hide();
+        //$('#' + containers[i]).removeClass('active');
+    }
+    
+    $('#' + container).show();
+    //$('#' + container).addClass('active');
+}
+
+
+
+
+
+var Router = Backbone.Router.extend({
+    routes: {
+        "help": "help",
+        "visit": "visit",
+        "patient": "patientIndex",
+        "patient/:id": "patientData",
+        "calendar": "calendar",
+        "report": "report",
+        "catalog": "catalog",
+        "search/:query": "search",
+        "search/:query/p:page": "search",
+        "dashboard": "dashboard",
+        "*path": "dashboard",
+    },
+    help: function() {
+        switchTo('dashboard');
+    },
+    search: function(query, page) {
+        console.log('Search for sth...');
+    },
+
+    patientIndex: function() {
+        switchTo('table_list');
+    },
+    patientData: function() {
+        switchTo('patient_data');
+    },
+    calendar: function() {
+        switchTo('calendar');
+    },
+    report: function() {
+        switchTo('table_list');
+    },
+    catalog: function() {
+        switchTo('table_list');
+    },
+    visit: function() {
+        switchTo('patient_data');
+    },
+    dashboard: function(path) {
+        switchTo('dashboard');
+    }
+});
+
+
